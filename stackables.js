bce1112a8dbecfad5238c8ead94ae99fa4871f23
Matthew Collier <collier.matthew@gmail.com>
2016-06-23 13:05:54 -0400

    Update copyright notice.

--- stackables.js
+++ stackables.js
@@ -1,7 +1,7 @@
 /*!
  * Stackables module.
  *
- * Copyright (c) 2014 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2014-2016 Digital Bazaar, Inc. All rights reserved.
  *
  * @author Dave Longley
  */
