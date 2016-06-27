e75870ad6941fcd5533ee15549461b73fa6e0904
Alex Lamar <alawi@vt.edu>
2016-06-27 12:30:32 -0400

    Fix pop-over positioning issue when document has been scrolled.

--- stackables.js
+++ stackables.js
@@ -520,9 +520,17 @@ function stackableTriggerDirective($parse) {
     }
 
     function updateState(state) {
+      // If we're using the dialog polyfil, .offset() will calculate the
+      // correct trigger position for us. If we aren't, .offset() needs
+      // to be subtracted from the document's scroll position. This
+      // fixes an issue we have with chrome, which doesn't use the polyfill.
       var offset = element.offset();
+      var scrollOffset = 0;
+      if(!usePolyfill) {
+        scrollOffset = angular.element(document).scrollTop();
+      }
       state.position = {
-        top: offset.top,
+        top: offset.top - scrollOffset,
         left: offset.left,
         height: element.outerHeight(false),
         width: element.outerWidth(false),
